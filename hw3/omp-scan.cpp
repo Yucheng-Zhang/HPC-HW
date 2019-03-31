0919214750654331a242fb8a9e90589e3ebbde1c
Yucheng Zhang <yczhang16807@icloud.com>
2019-03-31 16:19:06 -0400

    recover omp-scan.cpp

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -20,29 +20,30 @@ void scan_omp(long *prefix_sum, const long *A, long n) {
     return;
 
   int p, tid;
-  long ktid, ktid1;
+  long *k;
 
-#pragma omp parallel shared(prefix_sum, A, p, n) private(tid, ktid, ktid1)
+#pragma omp parallel shared(prefix_sum, A, p, n, k) private(tid)
   {
     tid = omp_get_thread_num();
     if (tid == 0) {
       p = omp_get_num_threads();
       printf(">> number of threads: %d\n", p);
+      k = (long *)malloc((p + 1) * sizeof(long));
+      for (int i = 0; i < p; i++) {
+        k[i] = i * (n / p);
+      }
+      k[p] = n;
     }
 #pragma omp barrier
-    ktid = tid * (n / p);
-    ktid1 = (tid + 1) * (n / p);
-    prefix_sum[ktid] = 0;
-    for (long i = ktid + 1; i < ktid1; i++) {
+    prefix_sum[k[tid]] = 0;
+    for (long i = k[tid] + 1; i < k[tid + 1]; i++) {
       prefix_sum[i] = prefix_sum[i - 1] + A[i - 1];
     }
   }
 
   for (int i = 1; i < p; i++) {
-    long ki = i * (n / p);
-    long ki1 = (i + 1) * (n / p);
-    long corr = prefix_sum[ki - 1] + A[ki - 1];
-    for (long j = ki; j < ki1; j++) {
+    long corr = prefix_sum[k[i] - 1] + A[k[i] - 1];
+    for (long j = k[i]; j < k[i + 1]; j++) {
       prefix_sum[j] += corr;
     }
   }
