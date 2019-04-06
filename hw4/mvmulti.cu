a9126bd0c45543b36285dfe8115dd2ba3a4dfce4
Yucheng Zhang <yczhang16807@icloud.com>
2019-04-06 17:27:50 -0400

    p1 vv multi

---
+++ hw4/mvmulti.cu
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <stdio.h>
+#include <omp.h>
+#include <string>
+
+void vvmulti(double* sum_ptr, const double* a, const double* b, long N){
+  double sum = 0;
+  #pragma omp parallel for schedule(static) reduction(+:sum)
+  for (long i = 0; i < N; i++) sum += a[i] * b[i];
+  *sum_ptr = sum;
+}
+
+#define BLOCK_SIZE 1024
+
+__global__ void vvmulti_kernel(double* sum, const double* a, const double* b, long N){
+  __shared__ double smem[BLOCK_SIZE];
+  int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+
+  if (idx < N) smem[threadIdx.x] = a[idx] * b[idx];
+  else smem[threadIdx.x] = 0;
+
+  __syncthreads();
+  if (threadIdx.x < 512) smem[threadIdx.x] += smem[threadIdx.x + 512];
+  __syncthreads();
+  if (threadIdx.x < 256) smem[threadIdx.x] += smem[threadIdx.x + 256];
+  __syncthreads();
+  if (threadIdx.x < 128) smem[threadIdx.x] += smem[threadIdx.x + 128];
+  __syncthreads();
+  if (threadIdx.x <  64) smem[threadIdx.x] += smem[threadIdx.x +  64];
+  __syncthreads();
+  if (threadIdx.x <  32) {
+    smem[threadIdx.x] += smem[threadIdx.x +  32];
+    __syncwarp();
+    smem[threadIdx.x] += smem[threadIdx.x +  16];
+    __syncwarp();
+    smem[threadIdx.x] += smem[threadIdx.x +   8];
+    __syncwarp();
+    smem[threadIdx.x] += smem[threadIdx.x +   4];
+    __syncwarp();
+    smem[threadIdx.x] += smem[threadIdx.x +   2];
+    __syncwarp();
+    if (threadIdx.x == 0) sum[blockIdx.x] = smem[0] + smem[1];
+  }
+}
+
+int main() {
+  long N = (1UL<<25);
+
+  /* Initialize two vectors */
+  double *a, *b;
+  cudaMallocHost((void**)&a, N*sizeof(double));
+  cudaMallocHost((void**)&b, N*sizeof(double));
+  #pragma omp parallel for schedule(static)
+  for (long i = 0; i < N; i++) {
+    a[i] = 1.0 / (i+1);
+    b[i] = 1.0;
+  }
+
+  /* CPU reference */
+  double sum_ref;
+  double tt = omp_get_wtime();
+  vvmulti(&sum_ref, a, b, N);
+  printf("CPU Bandwidth = %f GB/s\n", 2*N*sizeof(double) / (omp_get_wtime()-tt)/1e9);
+
+  /* GPU */
+  double *a_d, *b_d, *sum_d;
+  cudaMalloc(&a_d, N*sizeof(double));
+  cudaMalloc(&b_d, N*sizeof(double));
+  cudaMalloc(&sum_d, ((N+BLOCK_SIZE-1)/BLOCK_SIZE)*sizeof(double));
+  
+  /* Copy Host data to device */
+  cudaMemcpyAsync(a_d, a, N*sizeof(double), cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+  cudaMemcpyAsync(b_d, b, N*sizeof(double), cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+  tt = omp_get_wtime();
+
+  long Nb = (N+BLOCK_SIZE-1)/BLOCK_SIZE;
+  vvmulti_kernel<<<Nb,BLOCK_SIZE>>>(sum_d, a_d, b_d, N);
+  while (Nb > 1) {
+    long N = Nb;
+    Nb = (Nb+BLOCK_SIZE-1)/(BLOCK_SIZE);
+    vvmulti_kernel<<<Nb,BLOCK_SIZE>>>(sum_d+Nb, sum_d, N);
+    sum_d += Nb;
+  }
+
+  double sum;
+  cudaMemcpyAsync(&sum, sum_d, 1*sizeof(double), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  printf("GPU Bandwidth = %f GB/s\n", 2*N*sizeof(double) / (omp_get_wtime()-tt)/1e9);
+  printf("Error = %f\n", fabs(sum-sum_ref));
+
+  cudaFree(a_d);
+  cudaFree(b_d);
+  cudaFree(sum_d);
+  cudaFreeHost(a);
+  cudaFreeHost(b);
+
+  return 0;
+}
